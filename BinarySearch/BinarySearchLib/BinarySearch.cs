0d1a572d40c505dcd27c46eff9fce9446b645b91
Joshua Ball <joshua.g.s.ball@gmail.com>
2012-01-19 08:18:30 -0800

    First test - interface

--- BinarySearch/BinarySearchLib/BinarySearch.cs
+++ BinarySearch/BinarySearchLib/BinarySearch.cs
@@ -5,7 +5,11 @@ using System.Text;
 
 namespace BinarySearchLib
 {
-    public class Class1
+    public class BinarySearch
     {
+        public static int Find(List<int> numbers, int numToFind)
+        {
+            return 2;
+        }
     }
 }
--- BinarySearch/BinarySearchLibTest/BinarySearchTest.cs
+++ BinarySearch/BinarySearchLibTest/BinarySearchTest.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BinarySearchLib;
 using NUnit.Framework;
 
 namespace BinarySearchLibTest
 {
     [TestFixture]
-    public class Class1
+    public class BinarySearchTest
     {
         [Test]
         public void FindNumber()
         {
+            var numbers = new List<int>() { 1,2,3,4,5,6,7,8,9};
+
+            var position = BinarySearch.Find( numbers, 3 );
+            Assert.AreEqual( 2, position);
         }
     }
+
 }
